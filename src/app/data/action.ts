349889d25c20d386ee50ec5c3173d2a15974ba5f
nicky-speedline <nicky@speedlinesolutions.com>
2026-02-18 04:01:54 -0800

    Create basic form

---
+++ src/app/data/action.ts
@@ -0,0 +1,17 @@
+'use server';
+
+export async function postData(formData: FormData) {
+    const date = formData.get("date");
+    const avg = formData.get("avg");
+
+    const buy100m = formData.get("buy-100m");
+    const buy1b = formData.get("buy-1b");
+    const buy10b = formData.get("buy-10b");
+
+    const sell100m = formData.get("sell-100m");
+    const sell1b = formData.get("sell-1b");
+    const sell10b = formData.get("sell-10b");
+
+    const body = { date, avg, buy100m, buy1b, buy10b, sell100m, sell1b, sell10b };
+    console.log(JSON.stringify(body));
+}
