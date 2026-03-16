65b5ed77b1247f772b6b4a196922fdc37286c45a
nicky-speedline <nicky@speedlinesolutions.com>
2026-03-15 23:52:10 -0700

    Format form

--- src/app/components/manualInput.tsx
+++ src/app/components/manualInput.tsx
@@ -5,6 +5,7 @@ import {
   FormHelperText,
   Input,
   InputLabel,
+  TextField,
 } from "@mui/material";
 
 import { postData } from "../data/action";
@@ -14,7 +15,7 @@ export default function ManualInput() {
     <Box
       component="form"
       action={postData}
-      sx={{ "& > :not(style)": { m: 1 } }}
+      sx={{ display: 'inline-flex', flexDirection: 'column', alignItems: 'flex-start', gap: "1em" }}
       autoComplete="off"
     >
       <FormControl>
@@ -44,86 +45,97 @@ export default function ManualInput() {
           Average cost of Maple Points per 100M Mesos for recent trades
         </FormHelperText>
       </FormControl>
+      <Box
+        sx={{ display: 'flex', flexDirection: 'row', flexGrow: 1, gap: "1em" }}
+      >
+        <FormControl>
+          <InputLabel htmlFor="buy1B">Buy Buyout 1B</InputLabel>
+          <Input
+            id="buy1B"
+            name="buy1B"
+            type="number"
+            aria-describedby="buy1B-desc"
+          ></Input>
+          <FormHelperText id="buy1B-desc">
+            Cost to buyout 1B Mesos in Maple Points per 100M Mesos
+          </FormHelperText>
+        </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="buy10B">Buy Buyout 10B</InputLabel>
+          <Input
+            id="buy10B"
+            name="buy10B"
+            type="number"
+            aria-describedby="buy10B-desc"
+          ></Input>
+          <FormHelperText id="buy10B-desc">
+            Cost to buyout 10B Mesos in Maple Points per 100M Mesos
+          </FormHelperText>
+        </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="buy100M">Buy Buyout 100M</InputLabel>
+          <Input
+            id="buy100M"
+            name="buy100M"
+            type="number"
+            aria-describedby="buy100M-desc"
+          ></Input>
+          <FormHelperText id="buy100M-desc">
+            Cost to buyout 100M Mesos in Maple Points per 100M Mesos
+          </FormHelperText>
+        </FormControl>
+      </Box>
+      
+      <Box
+        sx={{ display: 'inline-flex', flexDirection: 'row', "& > :not(style)": { mr: 1 } }}
+      >
+       <FormControl>
+          <InputLabel htmlFor="sell100M">Sell Buyout 100M</InputLabel>
+          <Input
+            id="sell100M"
+            name="sell100M"
+            type="number"
+            aria-describedby="sell100M-desc"
+          ></Input>
+          <FormHelperText id="sell100M-desc">
+            Price to instantly sell 100M Mesos in Maple Points per 100M Mesos
+          </FormHelperText>
+        </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="sell1B">Sell Buyout 1B</InputLabel>
+          <Input
+            id="sell1B"
+            name="sell1B"
+            type="number"
+            aria-describedby="sell1B-desc"
+          ></Input>
+          <FormHelperText id="sell1B-desc">
+            Price to instantly sell 1B Mesos in Maple Points per 100M Mesos
+          </FormHelperText>
+        </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="sell10B">Sell Buyout 10B</InputLabel>
+          <Input
+            id="sell10B"
+            name="sell10B"
+            type="number"
+            aria-describedby="sell10B-desc"
+          ></Input>
+          <FormHelperText id="sell10B-desc">
+            Price to instantly sell 10B Mesos in Maple Points per 100M Mesos
+          </FormHelperText>
+        </FormControl>
+      </Box>
       <FormControl>
-        <InputLabel htmlFor="buy100M">Buy Buyout 100M</InputLabel>
-        <Input
-          id="buy100M"
-          name="buy100M"
-          type="number"
-          aria-describedby="buy100M-desc"
-        ></Input>
-        <FormHelperText id="buy100M-desc">
-          Cost to buyout 100M Mesos in Maple Points per 100M Mesos
-        </FormHelperText>
-      </FormControl>
-      <FormControl>
-        <InputLabel htmlFor="buy1B">Buy Buyout 1B</InputLabel>
-        <Input
-          id="buy1B"
-          name="buy1B"
-          type="number"
-          aria-describedby="buy1B-desc"
-        ></Input>
-        <FormHelperText id="buy1B-desc">
-          Cost to buyout 1B Mesos in Maple Points per 100M Mesos
-        </FormHelperText>
-      </FormControl>
-      <FormControl>
-        <InputLabel htmlFor="buy10B">Buy Buyout 10B</InputLabel>
-        <Input
-          id="buy10B"
-          name="buy10B"
-          type="number"
-          aria-describedby="buy10B-desc"
-        ></Input>
-        <FormHelperText id="buy10B-desc">
-          Cost to buyout 10B Mesos in Maple Points per 100M Mesos
-        </FormHelperText>
-      </FormControl>
-      <FormControl>
-        <InputLabel htmlFor="sell100M">Sell Buyout 100M</InputLabel>
-        <Input
-          id="sell100M"
-          name="sell100M"
-          type="number"
-          aria-describedby="sell100M-desc"
-        ></Input>
-        <FormHelperText id="sell100M-desc">
-          Price to instantly sell 100M Mesos in Maple Points per 100M Mesos
-        </FormHelperText>
-      </FormControl>
-      <FormControl>
-        <InputLabel htmlFor="sell1B">Sell Buyout 1B</InputLabel>
-        <Input
-          id="sell1B"
-          name="sell1B"
-          type="number"
-          aria-describedby="sell1B-desc"
-        ></Input>
-        <FormHelperText id="sell1B-desc">
-          Price to instantly sell 1B Mesos in Maple Points per 100M Mesos
-        </FormHelperText>
-      </FormControl>
-      <FormControl>
-        <InputLabel htmlFor="sell10B">Sell Buyout 10B</InputLabel>
-        <Input
-          id="sell10B"
-          name="sell10B"
-          type="number"
-          aria-describedby="sell10B-desc"
-        ></Input>
-        <FormHelperText id="sell10B-desc">
-          Price to instantly sell 10B Mesos in Maple Points per 100M Mesos
-        </FormHelperText>
-      </FormControl>
-      <FormControl>
-        <InputLabel htmlFor="notes">Notes</InputLabel>
-        <Input
+        <InputLabel htmlFor="notes"></InputLabel>
+        <TextField
           id="notes"
           name="notes"
-          type="text"
+          label="Notes"
+          multiline
+          maxRows={4}
           aria-describedby="notes-desc"
-        ></Input>
+        ></TextField>
         <FormHelperText id="notes-desc">Custom notes</FormHelperText>
       </FormControl>
       <Button variant="contained" color="success" type="submit">
